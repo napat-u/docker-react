bf299b63128f5a6878f35cf5c5718804dbcdaa4d
Napat <napat.udomlapsakul@gmail.com>
2020-09-16 13:46:19 +0700

    testing a deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GGOG
+          Hello Madacascar
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Testing
+          Potato Sky Line
         </a>
       </header>
     </div>
